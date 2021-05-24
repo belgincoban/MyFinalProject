cad587aa2c317d6483d4d8d38366cfb3e2366069
Belgin Çoban <belgin_coban@outlook.com>
2021-05-24 12:36:46 +0300

    24 mayıs eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -26,11 +26,22 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName +"/"+product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
-            Console.WriteLine("Hello World!");
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
+            
         }
     }
 }
